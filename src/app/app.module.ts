e15be8ae648929d2bc93ac714ed803d9743284c7
Rene de Leon <ralp_tamich@hotmail.com>
2018-03-22 22:07:09 -0600

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { pendientesComponent } from '../pages/pendientes/pendientes.components';
+import { terminadosComponent } from '../pages/teminados/terminados.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,9 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    pendientesComponent,
+    terminadosComponent,
     TabsPage
   ],
   imports: [
@@ -26,9 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    pendientesComponent,
+    terminadosComponent,
     TabsPage
   ],
   providers: [
---
+++ src/pages/pendientes/pendientes.components.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pendientes',
+  templateUrl: 'pendientes.components.html',
+})
+export class pendientesComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
